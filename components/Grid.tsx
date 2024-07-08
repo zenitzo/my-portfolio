101a32baad2994c5cb044f08d308297153444558
zenitzo <christiang.dev@gmail.com>
2024-07-08 16:44:14 -0400

    almost done template

---
+++ components/Grid.tsx
@@ -0,0 +1,26 @@
+import { gridItems } from '@/data';
+import { BentoGrid, BentoGridItem } from './ui/BentoGrid';
+
+const Grid = () => {
+  return (
+    <section id="about">
+      <BentoGrid className="w-full">
+        {gridItems.map((item, i) => (
+          <BentoGridItem
+            id={item.id}
+            key={i}
+            title={item.title}
+            description={item.description}
+            className={item.className}
+            img={item.img}
+            imgClassName={item.imgClassName}
+            titleClassName={item.titleClassName}
+            spareImg={item.spareImg}
+          />
+        ))}
+      </BentoGrid>
+    </section>
+  );
+};
+
+export default Grid;
