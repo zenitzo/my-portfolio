6952898f418d4de496254e40cbc6de91d687d40c
zenitzo <christiang.dev@gmail.com>
2024-07-11 17:45:15 -0400

    Update RecentProjects.tsx

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -23,7 +23,7 @@ const RecentProjects = () => {
                 <img
                   src={img}
                   alt={title}
-                  className="z-10 absolute bottom-0"
+                  className="z-10 absolute h-full w-full object-cover object-center overflow-hidden rounded-3xl"
                   // Project images above was added
                 />
               </div>
