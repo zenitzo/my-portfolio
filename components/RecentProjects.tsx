7f7071a4cdc474e0a395b0adcb56918944ca1ae4
zenitzo <christiang.dev@gmail.com>
2024-07-11 17:56:12 -0400

    Update RecentProjects.tsx

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -17,14 +17,13 @@ const RecentProjects = () => {
             className="sm:h-[41rem] h-[32rem] lg:min-h-[32.5rem] flex items-center justify-center sm:w-[570px] w-[90vw]">
             <PinContainer title={linkText} href={link}>
               <div className="relative flex items-center justify-center sm:w-[570px] w-[80vw] overflow-hidden sm:h-[40vh] h-[30vh]  mb-10">
-                <div className="relative w-full h-full overflow-hidden lg:rounded-3xl bg-[#13162d]">
-                  <img
-                    src={img}
-                    alt={title}
-                    className="z-10 absolute h-full w-full object-cover object-center overflow-hidden rounded-3xl"
-                    // Project images above was added
-                  />
-                </div>
+                <div className="relative w-full h-full overflow-hidden lg:rounded-3xl bg-[#13162d]"></div>
+                <img
+                  src={img}
+                  alt={title}
+                  className="z-10 absolute h-full w-full object-cover object-center overflow-hidden rounded-3xl"
+                  // Project images above was added
+                />
               </div>
               <h1 className="font-bold lg:text-2xl md:text-xl text-base line-clamp-1">
                 {title}
