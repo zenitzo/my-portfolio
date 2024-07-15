e898754a8d58281ed862c9c307ca7873ec165d03
zenitzo <christiang.dev@gmail.com>
2024-07-15 13:16:38 -0400

    favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/Z-4.png" type="image/png" />
+        <link rel="icon" href="/Z-4resize.png" type="image/png" />
       </head>
       <body className={inter.className}>
         <ThemeProvider
