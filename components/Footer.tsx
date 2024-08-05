5d22c27b61dccb6341740163782fc31bce86ac98
zenitzo <christiang.dev@gmail.com>
2024-08-05 14:09:30 -0400

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -10,7 +10,7 @@ const Footer = () => {
   };
 
   return (
-    <footer className="w-full pt-[2.5rem] mb-[100px] md:mb-5" id="contact">
+    <footer className="w-full mb-[100px] md:mb-5" id="contact">
       {/* <div className="flex flex-col items-center">
         <h1 className="heading lg:max-w-[45vw]">
           Ready to take <span className="text-purple">your</span> digital
